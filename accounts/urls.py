4f935241f09accfb8ecf158ef0c999dfd664ec2b
Jonathan Nzete <jonathannzete@gmail.com>
2021-10-11 16:03:34 +0100

    logout functionality added and working

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 # from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import AccountListAPI,UserAuth, UserRegister
+from .views import AccountListAPI,UserAuth, UserLogout, UserRegister
 
 
 urlpatterns = [
    path('', AccountListAPI.as_view(), name='account_list'),
    path('login/', UserAuth.as_view(), name="login"),
+   path('logout/', UserLogout.as_view(), name="logout"),
    path('register/', UserRegister.as_view(), name="register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token 
-from rest_framework.authtoken.views import ObtainAuthToken 
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from .models import Account
 from .serializers import AccountsSerializer, UserRegistrationSerializer
@@ -14,25 +14,27 @@ from . import signals
 
 
 class AccountListAPI(APIView):
-    
+
     def get(self, request):
         accounts = Account.objects.all()
         serializer = AccountsSerializer(accounts, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         accounts = Account.objects.all()
         serializer = AccountsSerializer(accounts, many=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors)
 
+
 class UserAuth(ObtainAuthToken):
-    
+
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer = self.serializer_class(
+            data=request.data, context={'request': request})
 
         data = {}
 
@@ -54,9 +56,18 @@ class UserAuth(ObtainAuthToken):
 
         return Response(data)
 
+
+class UserLogout(APIView):
+
+    def post(self, request):
+        token = Token.objects.get(user=request.user)
+        token.delete()
+        return Response({'logged out': "logout was successful!"}, status=status.HTTP_200_OK)
+
+
 class UserRegister(APIView):
     permission_classes = [permissions.AllowAny]
-    
+
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
 
@@ -73,8 +84,8 @@ class UserRegister(APIView):
             data['email'] = account.email
             data['first_name'] = account.first_name
             data['last_name'] = account.last_name
-            
+
         else:
-            data = serializer.errors # populate data with serializers errors
+            data = serializer.errors  # populate data with serializers errors
 
         return Response(data)
